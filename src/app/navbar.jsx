0c600aa2a299623273096b40cb55b8d27c91e757
= <=>
2024-05-07 07:44:51 -0400

    Feat: Primera parte del home lista

--- src/app/navbar.jsx
+++ src/app/navbar.jsx
@@ -1,32 +1,40 @@
-import "./navbar.css"
-import Link from "next/link"
-export default function Navbar() {
+import { useState, useEffect } from "react";
+import "./navbar.css";
+import Link from "next/link";
 
-    return <header>
+export default function Navbar({ scroll }) {
+  const [headerClass, setHeaderClass] = useState("headerClass");
+
+  useEffect(() => {
+    if (scroll === 0) {
+      setHeaderClass("headerClass");
+    } else {
+      setHeaderClass("headerWhite");
+    }
+  }, [scroll]);
+
+  return (
+    <header className={headerClass}>
+      <div>
+        <img src="https://gopharma.com.ve/wp-content/uploads/2024/04/Logo-GoPharma-Rif-Vector.png" width={"50px"} />
+      </div>
+      <div id="header_2">
         <div>
-            <img src="https://gopharma.com.ve/wp-content/uploads/2024/04/Logo-GoPharma-Rif-Vector.png" width={"50px"}></img>
+          <Link href="/">Inicio</Link>
         </div>
-        <div id="header_2">
-            <div>
-                asd
-            </div>
-            <div>
-                <Link href="/">Inicio</Link>
-            </div>
-            <div>
-                <Link href="/">Sobre Nosotros</Link>
-            </div>
-            <div>
-                <Link href="/">Servicios</Link>
-            </div>
-            <div>
-                <Link href="/">App Movil</Link>
-            </div>
-            <div>
-                <Link href="/">Whatsapp</Link>
-            </div>
+        <div>
+          <Link href="/about">Sobre Nosotros</Link>
         </div>
+        <div>
+          <Link href="/services">Servicios</Link>
+        </div>
+        <div>
+          <Link href="/movilapp">App Movil</Link>
+        </div>
+        <div>
+          <Link href="/whatsapp">Whatsapp</Link>
+        </div>
+      </div>
     </header>
-
-
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,33 @@
-export default function HomePage(){
-  return <div>
+import Link from "next/link";
+import "./page.css";
+import { Button, Flex } from "antd";
 
-
-
-
-  </div>
+export default function HomePage() {
+  return (
+    <div id="firstDiv">
+      <div id="firstDiv_1">
+        <div id="firstDiv_1_1"># EL SOFTWARE N° 1 EN VENEZUELA</div>
+        <div id="firstDiv_1_2">
+          GoPharma es una empresa enfocada en impulsar el crecimiento de las
+          farmacias independientes en Venezuela.
+        </div>
+        <div id="firstDiv_1_3">
+          La gestión de medicamentos nunca había sido tan sencilla y segura.
+          Descarga nuestra aplicación móvil y comienza a disfrutar de los
+          beneficios de comprar tus medicamentos desde la comodidad de tu hogar,
+          ahorrando tiempo y con la tranquilidad de estar adquiriendo productos
+          de calidad.
+        </div>
+        <div id="firstDiv_1_4">
+          <Button type="primary" className="ButtonsInicio">
+            <Link href="/about">Sobre Nosotros</Link>
+          </Button>
+          <Button type="primary" className="ButtonsInicio">
+            <Link href="/services">Nuestros Servicios</Link>
+          </Button>
+        </div>
+      </div>
+      <div id="firstDiv_2"></div>
+    </div>
+  );
 }
